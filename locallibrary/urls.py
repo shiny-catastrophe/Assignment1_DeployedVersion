c14e90c7b5bf1b1416bd02ba48b3256a8fa16e30
shiny-catastrophe <aimee.r.evans@gmail.com>
2025-03-03 21:29:35 -0600

    Add files via upload

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -21,8 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^media/(?P<path>.*)$)', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$'), serve, {'document_root': settings.STATIC_ROOT},
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('', include('catalog.urls')),
 path('accounts/', include('django.contrib.auth.urls')),
 ]
